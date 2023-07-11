74580467bd6f593b3712a9887067218330cbe55c
abuzar-shaikh <110722376+abuzar-shaikh@users.noreply.github.com>
2023-07-11 09:02:35 +0530

    mbalayang-project

---
+++ README.md
@@ -0,0 +1,9 @@
+# MbalayangProject
+
+I have cloned a e-commarce application called Mbalayang.com It simply meant allowing to Book a Hotel. Over time it has evolved to provide multiple benefits on one platform for all.
+
+In this Project, I have tried to implement some of the functionalities of the application like search and along with these I have tried sorting functionalities and the User flow of booking a hotel and applying best available offers.
+
+This is a Construct Week Project.
+
+VERCEL LINK - 
---
+++ src/Components/Login.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Button,
+  InputGroup,
+  InputRightElement,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import Style from "./Login.css";
+import video1 from "../assets/video4.mp4";
+import { Authcontext } from "../context/Authcontext";
+
+const Login = () => {
+  const [show, setShow] = React.useState(false);
+  const navigate = useNavigate();
+  const [state, setState] = useState({ email: "", password: "" });
+  const [data, setData] = useState([]);
+  const handleClick = () => setShow(!show);
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+  // const handelSubmit = (e) => {
+  //   e.preventDefault();
+  //   return new Promise(async (resolve, reject) => {
+  //     const response = await fetch(`http://localhost:8080/users`);
+  //     const data = await response.json();
+  //     var count = 0;
+  //     data.forEach((user) => {
+  //       if (user.email === state.email && user.password === state.password) {
+  //         count++;
+  //       }
+  //     });
+  //     if (count > 0) {
+  //       alert("Login successfully");
+  //       setTimeout(() => {
+  //         navigate("/");
+  //       }, 1000);
+  //     } else {
+  //       alert("! Please check your email or Password");
+  //     }
+  //   });
+  // };
+  const { auth, setAuth, log, setLog } = useContext(Authcontext);
+  const handelSubmit = (e) => {
+    e.preventDefault();
+
+    if (auth.email === state.email && auth.password === state.password) {
+      alert("Login successfully");
+      setLog(true);
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
+    } else {
+      alert("Please check your email or Password");
+    }
+  };
+  console.log(auth);
+  return (
+    <div className="mainLogin">
+      <div className="overlayLogin"></div>
+      <video className="videoLogin" src={video1} autoPlay muted loop />
+      <div className="containerLogin">
+        <form className="formLogin" onSubmit={handelSubmit}>
+          <Heading as="h4" size="lg">
+            Login
+          </Heading>
+          &nbsp;&nbsp;
+          <FormControl>
+            <FormLabel> Email Address</FormLabel>
+            <Input
+              name="email"
+              placeholder="Enter Email"
+              onChange={handleChange}
+            />
+          </FormControl>
+          &nbsp;
+          <FormLabel>Password</FormLabel>
+          <FormControl isRequired>
+            <InputGroup size="md">
+              <Input
+                name="password"
+                pr="4.5rem"
+                type={show ? "text" : "password"}
+                placeholder="4+characters"
+                onChange={handleChange}
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleClick}>
+                  {show ? "Hide" : "Show"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+          <input type="submit" value={"Login"} className="signbtn" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
